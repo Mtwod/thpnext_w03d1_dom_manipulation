9b69977c18f5d5aacde37e2b873e028bfc5d933e
Mtwod <maximewong@hotmail.com>
2021-01-18 16:27:49 +0100

    Add random menu

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ const homeContent = document.getElementById('home').innerHTML;
 const menuContent = document.getElementById('menuContent').innerHTML;
 
 
+/******************************
+ * THP MENU ALÉATOIRE
+ *******************************/
+
+var main_courses = ["Filet de turbot de la mer Noire", "Tablier de sapeur", "Gigot d'agneau", "Faisan de forêt", "Trio de quinoa, chou kale et pousses d'épinard"]
+var techniques = ["à la cocotte", "minute", "avec sa sauce hollandaise", "façon sud-ouest", "comme chez ma grand-mère", "déglacé au saké", "maturé en fût de chêne"]
+var sides = ["une purée de topinambour", "ses frites truffées", "des châtaignes croustillantes", "une brunoise carotte-cèleri", "un oeuf parfait", "sa crème veloutée de fromages affinés"]
+var seasonings = ["au yuzu rouge", "au poivre vert de Sichuan", "et une pointe de safran", "à l'ail noir", "et un peu de sucre en poudre"]
+
+var random_main_course = main_courses[Math.floor(Math.random()*main_courses.length)]
+var random_technique = techniques[Math.floor(Math.random()*techniques.length)]
+var random_side = sides[Math.floor(Math.random()*sides.length)]
+var random_seasoning = seasonings[Math.floor(Math.random()*seasonings.length)]
+
+var randomMenu = `${random_main_course} ${random_technique}, avec ${random_side} ${random_seasoning}`
+
+
+
 const clickHome = (e) => {
   e.preventDefault();
   const homeLink = document.getElementById('homeLink');
@@ -14,7 +32,7 @@ const clickHome = (e) => {
     homeLink.style.border = '3px solid red';
     document.getElementById('content').innerHTML = `<div id='home'>${homeContent}</div>`;
   }
-}
+};
 
 
 const clickMenu = (e) => {
@@ -27,11 +45,32 @@ const clickMenu = (e) => {
     menuLink.style.color = 'red';
     menuLink.style.border = '3px solid red';
     document.getElementById('content').innerHTML = menuContent;
+    const buttonMixMenu = document.getElementById('mixMenu');
+    buttonMixMenu.addEventListener('click', clickMixMenu);
   }
-}
+};
 
 const homeLink = document.getElementById('homeLink');
 const menuLink = document.getElementById('menuLink');
+
+
+const clickMixMenu = (e) => {
+  e.preventDefault();
+  random_main_course = main_courses[Math.floor(Math.random()*main_courses.length)]
+  random_technique = techniques[Math.floor(Math.random()*techniques.length)];
+  random_side = sides[Math.floor(Math.random()*sides.length)];
+  random_seasoning = seasonings[Math.floor(Math.random()*seasonings.length)];
+
+  randomMenu = `${random_main_course} ${random_technique}, avec ${random_side} ${random_seasoning}`;
+
+  const menuLink = document.getElementById('menuLink');
+  const menuElement = document.getElementById('menu');
+  menuLink.style.color = 'yellow';
+  menuLink.style.border = '5px solid green';
+  menuElement.innerHTML = `ET BAH VOILÀ : <br> <h3>${randomMenu}</h3>`;
+};
+
+
 
 homeLink.addEventListener('click', clickHome);
 menuLink.addEventListener('click', clickMenu);
